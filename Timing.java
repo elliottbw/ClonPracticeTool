3591a574439d6cf2d2e3593d3c3dec93b4e1f42d
Elliott Weaver <me@elliottweaver.com>
2020-06-02 00:33:54 +0100

    Initial commit to Timing class
    A tuple for representing the beginning and ending points of sections

---
+++ Timing.java
@@ -0,0 +1,25 @@
+public class Timing {
+	private int start = 0;
+	private int end = 0;
+
+	public Timing(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+}
